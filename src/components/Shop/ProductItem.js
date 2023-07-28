64298399ce76ef6e86638df59df721d718401d2c
harshhj17 <harshjaiswal857@gmail.com>
2023-07-28 14:56:26 +0530

    section 20

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -5,34 +5,37 @@ import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const { title, price, description, id } = props;
+    const { title, price, description, id } = props;
 
-  const addToCartHandler = () => {
-    dispatch(
-        cartActions.addItemToCart({
-          id,
-          title,
-          price,
-        })
+    const addToCartHandler = () => {
+        // and then send Http request
+        // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+        dispatch(
+            cartActions.addItemToCart({
+                id,
+                title,
+                price,
+            })
+        );
+    };
+
+    return (
+        <li className={classes.item}>
+            <Card>
+                <header>
+                    <h3>{title}</h3>
+                    <div className={classes.price}>${price.toFixed(2)}</div>
+                </header>
+                <p>{description}</p>
+                <div className={classes.actions}>
+                    <button onClick={addToCartHandler}>Add to Cart</button>
+                </div>
+            </Card>
+        </li>
     );
-  };
-
-  return (
-      <li className={classes.item}>
-        <Card>
-          <header>
-            <h3>{title}</h3>
-            <div className={classes.price}>${price.toFixed(2)}</div>
-          </header>
-          <p>{description}</p>
-          <div className={classes.actions}>
-            <button onClick={addToCartHandler}>Add to Cart</button>
-          </div>
-        </Card>
-      </li>
-  );
 };
 
 export default ProductItem;
